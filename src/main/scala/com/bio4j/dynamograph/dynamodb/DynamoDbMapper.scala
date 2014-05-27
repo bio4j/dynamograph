014af0d6c8b52335be0527dc9b563be210a67488
balberski <alberskib@gmail.com>
2014-05-28 00:09:43 +0200

    [#14] - Introduce DynamoElementIdentifier

--- src/main/scala/com/bio4j/dynamograph/dynamodb/DynamoDbMapper.scala
+++ src/main/scala/com/bio4j/dynamograph/dynamodb/DynamoDbMapper.scala
@@ -1,18 +1,18 @@
 package com.bio4j.dynamograph.dynamodb
 
 import com.amazonaws.services.dynamodbv2.model.AttributeValue
-import com.bio4j.dynamograph.{DynamoRawEdge, DynamoRawVertex}
+import com.bio4j.dynamograph.{DynamoElementIdentifier, DynamoRawEdge, DynamoRawVertex}
 
 
 class DynamoDbMapper {
   // TODO: BA provide proper implementation
   def mapToDynamoRawVertex(values: Map[String, AttributeValue]) : DynamoRawVertex = {
-    val dummyResult = DynamoRawVertex("test", "id", values);
+    val dummyResult = DynamoRawVertex(DynamoElementIdentifier("id","test"),values);
     dummyResult
   }
 
   def mapToDynamoRawEdge(values: Map[String, AttributeValue]) : DynamoRawEdge = {
-    val dummyResult = DynamoRawEdge("test", "id","target", "source", values);
+    val dummyResult = DynamoRawEdge(DynamoElementIdentifier("id","test"),DynamoElementIdentifier("id","target"),DynamoElementIdentifier("id","source"), values);
     dummyResult
   }
 
