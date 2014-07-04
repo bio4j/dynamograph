a8d4d8923e01aef72a68ea06b040f5dbfc7b6330
balberski <alberskib@gmail.com>
2014-07-04 21:37:32 +0200

    [#37] Improvements for AnyDynamoDbExecutor

--- build.sbt
+++ build.sbt
@@ -13,6 +13,7 @@ scalaVersion := "2.11.1"
 libraryDependencies ++= Seq(
   "ohnosequences" %% "scarph" % "0.1.0",
   "ohnosequences" %% "tabula" % "0.1.0-SNAPSHOT",
+  //"com.typesafe.scala-logging" %% "scala-logging-slf4j" % "3.0.0", currentlu unavailable for 2.11
   "com.amazonaws" % "aws-java-sdk" % "1.8.0",
   "org.scala-lang.modules" %% "scala-xml" % "1.0.2",
   "org.scalatest" %% "scalatest" % "2.1.6" % "test"
--- src/main/scala/com/bio4j/dynamograph/dynamodb/DynamoDbExecutor.scala
+++ src/main/scala/com/bio4j/dynamograph/dynamodb/DynamoDbExecutor.scala
@@ -4,15 +4,17 @@ import com.amazonaws.services.dynamodbv2.model._
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
+import com.amazonaws.{AmazonClientException, AmazonServiceException}
 
 
 class DynamoDbExecutor(val ddb: AmazonDynamoDB) extends AnyDynamoDbExecutor {
 
 
-  override def execute(request: QueryRequest): List[Map[String, AttributeValue]] = {
+  override def execute(request: QueryRequest): List[Map[String, AttributeValue]] = withinTry {
     var result : QueryResult = ddb.query(request)
     val resultList = result.getItems
     while (result.getLastEvaluatedKey !=null){
+      //TODO: exponential backoff?
       request.withExclusiveStartKey(result.getLastEvaluatedKey)
       result = ddb.query(request)
       resultList ++= result.getItems
@@ -21,10 +23,11 @@ class DynamoDbExecutor(val ddb: AmazonDynamoDB) extends AnyDynamoDbExecutor {
   }
 
 
-  override def execute(request: BatchGetItemRequest): List[Map[String, AttributeValue]] =  {
+  override def execute(request: BatchGetItemRequest): List[Map[String, AttributeValue]] = withinTry {
     var result = ddb.batchGetItem(request)
     var resultList = flattenResult(result)
     while (result.getUnprocessedKeys.size() > 0){
+      //TODO: exponential backoff?
       request.withRequestItems(result.getUnprocessedKeys)
       result = ddb.batchGetItem(request)
       resultList ++= flattenResult(result)
@@ -32,11 +35,11 @@ class DynamoDbExecutor(val ddb: AmazonDynamoDB) extends AnyDynamoDbExecutor {
     resultList.map(x => x.asScala.toMap).toList
   }
 
-  override def execute(request: GetItemRequest): Map[String, AttributeValue] = {
-      ddb.getItem(request).getItem match {
-        case null => Map()
-        case item: java.util.Map[String,AttributeValue] => item.asScala.toMap
-      }
+  override def execute(request: GetItemRequest): Map[String, AttributeValue] = withinTry {
+    ddb.getItem(request).getItem match {
+      case null => Map()
+      case item: java.util.Map[String,AttributeValue] => item.asScala.toMap
+    }
   }
 
   private def flattenResult(batchResult : BatchGetItemResult) = for {
@@ -44,12 +47,21 @@ class DynamoDbExecutor(val ddb: AmazonDynamoDB) extends AnyDynamoDbExecutor {
     singleValue <- values
   } yield singleValue
 
-  private def withinTry[A](f :() =>A): A = {
+  private def withinTry[A](f: => A): A = {
     try{
-      f()
-    } catch{
-      case e: Throwable => {
-        throw e
+      f
+    } catch {
+      case ex @ (_ : ResourceNotFoundException | _ : ProvisionedThroughputExceededException | _ : InternalServerErrorException) => {
+        //TODO: BA logging
+        throw ex
+      }
+      case ex: AmazonServiceException => {
+        //TODO: BA logging
+        throw ex
+      }
+      case ex: AmazonClientException => {
+        //TODO: BA logging
+        throw ex
       }
     }
   }
