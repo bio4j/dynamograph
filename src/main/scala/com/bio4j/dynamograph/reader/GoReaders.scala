af036296d7304b62c30eec17fce81497b17935ee
balberski <alberskib@gmail.com>
2014-08-11 19:43:11 +0200

    [#18] Remove edge readers

--- src/main/scala/com/bio4j/dynamograph/reader/GoReaders.scala
+++ src/main/scala/com/bio4j/dynamograph/reader/GoReaders.scala
@@ -11,30 +11,4 @@ object GoReaders {
 
   case object goTermVertexReader            extends VertexReader(GoTermTable)
   case object goNamespaceVertexReader       extends VertexReader(GoNamespacesTable)
-
-  case object isAEdgeReader                 extends EdgeReader(IsATables, ServiceProvider.dynamoDbExecutor)
-  case object hasPartEdgeReader             extends EdgeReader(HasPartTables, ServiceProvider.dynamoDbExecutor)
-  case object partOfEdgeReader              extends EdgeReader(PartOfTables, ServiceProvider.dynamoDbExecutor)
-  case object regulatesEdgeReader           extends EdgeReader(RegulatesTables, ServiceProvider.dynamoDbExecutor)
-  case object negativelyRegulatesEdgeReader extends EdgeReader(NegativelyRegulatesTables, ServiceProvider.dynamoDbExecutor)
-  case object positivelyRegulatesEdgeReader extends EdgeReader(PositivelyRegulatesTables, ServiceProvider.dynamoDbExecutor)
-  case object namespaceEdgeReader           extends EdgeReader(NamespaceTables, ServiceProvider.dynamoDbExecutor)
-
-  def reader[VT <: Singleton with AnyVertexTypeWithId](vertex : VT) : AnyVertexReader.withVertexType[VT] = vertexReaders(vertex).asInstanceOf[AnyVertexReader.withVertexType[VT]]
-  
-  val vertexReaders = Map[AnyVertexTypeWithId, AnyVertexReader] (
-    GoTermType       -> goTermVertexReader,
-    GoNamespacesType -> goNamespaceVertexReader
-  )
-
-  val edgeReaders = Map[AnyDynamoEdge, AnyEdgeReader](
-    Namespace            -> namespaceEdgeReader,
-    PositivelyRegulates  -> positivelyRegulatesEdgeReader,
-    NegativelyRegulates  -> negativelyRegulatesEdgeReader,
-    Regulates            -> regulatesEdgeReader,
-    PartOf               -> partOfEdgeReader,
-    HasPart              -> hasPartEdgeReader,
-    IsA                  -> isAEdgeReader
-  )
-
 }
